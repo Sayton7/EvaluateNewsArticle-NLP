1701eff97e08177da75a555e443030ebc40266c1
Karim Ayman <nighthawk1486@gmail.com>
2021-10-21 14:59:47 +0200

    Implementing calling the api from the server side using the input from the client side

--- src/client/js/textAnalyzer.js
+++ src/client/js/textAnalyzer.js
@@ -1,5 +1,26 @@
 export function textAnalyzer(e) {
     e.preventDefault()
 
-    let text = document.getElementById('text').value
+    let url = document.getElementById('url').value
+
+    postText('/analyze', {url: url})
+}
+
+const postText = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+
+    try {
+        const newData = await response.json()
+        console.log(newData)
+        return newData
+    } catch (err) {
+        console.log("error", err)
+    }
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,15 @@
 require('dotenv').config()
 var path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
 const mockAPIResponse = require('./mockAPI.js')
+const fetch = require("node-fetch")
+const FormData = require('form-data')
 
 const app = express()
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.use(express.static('dist'))
 
@@ -22,3 +28,23 @@ app.listen(8081, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+// Setting up the API POST request
+
+
+
+// const formdata = {
+//     "key": process.env.API_KEY,
+//     "lang": "en",
+// }
+
+app.post('/analyze', textToAnalyze)
+  
+function textToAnalyze(req, res) {
+    const url = `https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&lang=en&url=${req.body.url}`
+    const options = {
+        'method': 'POST'
+      }
+    fetch(url, options)
+      .then(data => data.json()).then(data => res.send(data))
+}
