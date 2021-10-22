017495f1c587408b73cb91409c4c151c13e0a87a
Karim Ayman <nighthawk1486@gmail.com>
2021-10-22 22:35:54 +0200

    Adding workbox service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
 const TerserPlugin = require("terser-webpack-plugin")
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
     entry: ['@babel/polyfill', './src/client/index.js'],
@@ -41,6 +42,12 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new MiniCssExtractPlugin({ filename: "[name].css" })
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
+        new WorkboxPlugin.GenerateSW({
+          // these options encourage the ServiceWorkers to get in there fast
+          // and not allow any straggling "old" SWs to hang around
+          clientsClaim: true,
+          skipWaiting: true,
+        })
     ]
 }
